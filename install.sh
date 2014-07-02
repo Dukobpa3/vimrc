baec13c64266927308d8b5c9c4d321cc855f2efa
dukobpa3 <a.burdun@gmail.com>
2014-07-02 11:24:35 +0000

    updated on zonk stage

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
-cp ./.vimrc ~/.vimrc
+#!/bin/bash -
+#title			:install.sh
+#description	:This script will install vimrc for current user.
+#author			:dukobpa3
+#date			:20140702
+#version		:0.1
+#usage			:bash install.sh
+
+cp ./vimrc ~/.vimrc
+cp -r ./vim/* ~/.vim/
+
